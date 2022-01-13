46edc5ae7ef9ed3b85df2310cca87e6e4c2d0cae
Affaxed Kiprotich <delhinrharl@gmail.com>
2022-01-13 16:36:46 +0100

    Fix linter errors

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import './App.css';
 import Calculator from './Components/calculator';
-class App extends React.Component{
-    constructor(props) {
+
+class App extends React.Component {
+  constructor(props) {
     super(props);
     this.state = {};
   }
