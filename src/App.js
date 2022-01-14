23b2a940147b3ca74fa3dcc501ba27824df3f8be
Affaxed Kiprotich <delhinrharl@gmail.com>
2022-01-14 09:35:15 +0100

    Refactor from class to hooks

--- src/App.js
+++ src/App.js
@@ -2,16 +2,7 @@ import React from 'react';
 import './App.css';
 import Calculator from './Components/calculator';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  render() {
-    return (
-      <Calculator />
-    );
-  }
-}
+const App = () => (
+  <Calculator />
+);
 export default App;
