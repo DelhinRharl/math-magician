6ee522b93e176206d25a3361059647696dec4030
Affaxed Kiprotich <delhinrharl@gmail.com>
2022-01-18 14:08:55 +0100

    Add other pages for website

--- src/App.js
+++ src/App.js
@@ -1,8 +1,34 @@
 import React from 'react';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
 import Calculator from './Components/calculator';
+import Navigation from './Components/Navigation';
+import Home from './Components/Home';
+import Quotes from './Components/Quotes';
 
 const App = () => (
-  <Calculator />
+  <>
+    <Navigation />
+    <Routes>
+      <Route
+        path="/"
+        element={(
+          <Home />
+        )}
+      />
+      <Route
+        path="/calculator"
+        element={(
+          <Calculator />
+        )}
+      />
+      <Route
+        path="/Quotes"
+        element={(
+          <Quotes />
+        )}
+      />
+    </Routes>
+  </>
 );
 export default App;
